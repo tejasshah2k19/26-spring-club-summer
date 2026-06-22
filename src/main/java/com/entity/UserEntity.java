d83d597245bf9ca9736f0b9ddfaa9e4f9fa91375
Tejas Shah <tejasshah2k19@gmail.com>
2026-06-22 12:51:43 +0530

    hb , entity

---
+++ src/main/java/com/entity/UserEntity.java
@@ -0,0 +1,86 @@
+package com.entity;
+
+import java.time.LocalDate;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity // create table
+@Table(name = "users") // table name
+public class UserEntity {
+
+	// columns --- instance variables
+	//primary ?
+	
+	
+	@Id // primary key
+	@GeneratedValue(strategy = GenerationType.IDENTITY)// auto increment
+	private Integer userId;
+	
+	private String firstName;
+	private String lastName;
+	private String email;
+	private String password;
+	private String role;
+	private LocalDate createdAt;
+
+	public Integer getUserId() {
+		return userId;
+	}
+
+	public void setUserId(Integer userId) {
+		this.userId = userId;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getRole() {
+		return role;
+	}
+
+	public void setRole(String role) {
+		this.role = role;
+	}
+
+	public LocalDate getCreatedAt() {
+		return createdAt;
+	}
+
+	public void setCreatedAt(LocalDate createdAt) {
+		this.createdAt = createdAt;
+	}
+
+}
